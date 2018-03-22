32a2ddff0d3a57c7569f77ec26855163072d99eb
folayanc <folayanc@hotmail.com>
2018-03-21 20:58:25 -0400

    secondserver

--- backend/webserver.js
+++ backend/webserver.js
@@ -1,11 +1,29 @@
-var path = require('path');
-var express = require('express');
+const express = require ('express');
+const app = express();
+const path = require ('path');
+const bodyParser = require('body-parser');
+const index = require ('./router/index');
+const port = ('port', process.env.PORT||3000);
 
-var app = express();
-
-var staticPath = path.join(__dirname, '/public');
-app.use(express.static(staticPath));
-
-app.listen(3000, function() {
-  console.log('listening');
+//database connection  
+var mongoDB = require ('mongoDB');
+mongoDB.connect ('mongodb://<dbuser>:<dbpassword>@ds121599.mlab.com:21599/shiftup');
+//set view engine
+app.set ('view engine','ejs');
+// set views
+app.set ('views', __dirname + '/views');
+//json reader
+app.use (bodyParser.json());
+//  middleware 
+app.use(bodyParser.urlencoded({ extended: true }));
+// serve static files
+app.use (express.static ('public'));
+// homepage 
+app.use ('/',index);
+//listen to port
+app.listen (3000, function() {
+  console.log ("Server is running");
 });
+//function that runs server: test server at http://localhost:3000
+node.server.js
+
